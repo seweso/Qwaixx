dc69eed69047a7fce9017ae8d2fcd68babc56b78
Wouter Schut <github@seweso.com>
2023-10-16 15:27:03 +0200

    Update streamlit_app.py - Add dice

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,18 +5,18 @@ class Player:
     def __init__(self, name):
         self.name = name
 
-    def make_move(self, game_state):
+    def make_move(self, game_state, dice_result):
         pass
 
 class HumanPlayer(Player):
-    def make_move(self, game_state):
+    def make_move(self, game_state, dice_result):
+        st.write(f"{self.name}, it's your turn. Dice result: {dice_result}")
         # Implement the logic for a human player's move
-        st.write(f"{self.name}, it's your turn. Choose a move:")
 
 class AiPlayer(Player):
-    def make_move(self, game_state):
+    def make_move(self, game_state, dice_result):
+        st.write(f"{self.name} is making a move... Dice result: {dice_result}")
         # Implement the logic for an AI player's move
-        st.write(f"{self.name} is making a move...")
 
 # Example game state
 game_state = {
@@ -35,7 +35,10 @@ players = [human_player, ai_player]
 # Streamlit app
 st.title("Qwixx Game Simulator")
 
-for turn in range(10):  # Simulating 10 turns for demonstration
+if st.button("Roll Dice"):
+    dice_result = random.randint(1, 6)
+    st.write(f"Dice result: {dice_result}")
+    
     current_player = random.choice(players)  # Choose a random player
     game_state["current_player"] = current_player
-    current_player.make_move(game_state)
+    current_player.make_move(game_state, dice_result)
