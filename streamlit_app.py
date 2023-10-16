3c07b8719ce6d8676d120a9d8cc74171ecf4819a
Wouter Schut <github@seweso.com>
2023-10-16 15:25:10 +0200

    Update streamlit_app.py - Players

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,41 @@
 import streamlit as st
+import random
 
+class Player:
+    def __init__(self, name):
+        self.name = name
 
-st.write("Hello Qwaixx")
+    def make_move(self, game_state):
+        pass
+
+class HumanPlayer(Player):
+    def make_move(self, game_state):
+        # Implement the logic for a human player's move
+        st.write(f"{self.name}, it's your turn. Choose a move:")
+
+class AiPlayer(Player):
+    def make_move(self, game_state):
+        # Implement the logic for an AI player's move
+        st.write(f"{self.name} is making a move...")
+
+# Example game state
+game_state = {
+    "current_player": None,
+    "board": [0, 0, 0, 0, 0],  # Represents the Qwixx game board
+    # Add more game state variables as needed
+}
+
+# Create players
+human_player = HumanPlayer("Alice")
+ai_player = AiPlayer("AI Bot")
+
+# List of players
+players = [human_player, ai_player]
+
+# Streamlit app
+st.title("Qwixx Game Simulator")
+
+for turn in range(10):  # Simulating 10 turns for demonstration
+    current_player = random.choice(players)  # Choose a random player
+    game_state["current_player"] = current_player
+    current_player.make_move(game_state)
