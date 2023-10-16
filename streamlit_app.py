9ceb6ea9f4925ce8351cd7eff4e040b72f5e38c3
Wouter Schut <github@seweso.com>
2023-10-16 15:29:54 +0200

    Update streamlit_app.py - More die

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,17 +5,17 @@ class Player:
     def __init__(self, name):
         self.name = name
 
-    def make_move(self, game_state, dice_result):
+    def make_move(self, game_state, dice_results):
         pass
 
 class HumanPlayer(Player):
-    def make_move(self, game_state, dice_result):
-        st.write(f"{self.name}, it's your turn. Dice result: {dice_result}")
+    def make_move(self, game_state, dice_results):
+        st.write(f"{self.name}, it's your turn. Dice results: {dice_results}")
         # Implement the logic for a human player's move
 
 class AiPlayer(Player):
-    def make_move(self, game_state, dice_result):
-        st.write(f"{self.name} is making a move... Dice result: {dice_result}")
+    def make_move(self, game_state, dice_results):
+        st.write(f"{self.name} is making a move... Dice results: {dice_results}")
         # Implement the logic for an AI player's move
 
 # Example game state
@@ -35,10 +35,12 @@ players = [human_player, ai_player]
 # Streamlit app
 st.title("Qwixx Game Simulator")
 
-if st.button("Roll Dice"):
-    dice_result = random.randint(1, 6)
-    st.write(f"Dice result: {dice_result}")
+if st.button("Roll All Dice"):
+    dice_results = [random.randint(1, 6) for _ in range(6)]
+    st.write("Dice results:")
+    for i, result in enumerate(dice_results):
+        st.write(f"Dice {i+1}: {result}")
     
     current_player = random.choice(players)  # Choose a random player
     game_state["current_player"] = current_player
-    current_player.make_move(game_state, dice_result)
+    current_player.make_move(game_state, dice_results)
