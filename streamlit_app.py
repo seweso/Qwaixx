1a90e0be53d451c2c20e2a3d873a15e442ebcde1
Wouter Schut <github@seweso.com>
2023-10-16 15:37:50 +0200

    Update streamlit_app.py - You forgot the dice numbers

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,17 +6,19 @@ class Player:
         self.name = name
 
     def make_move(self, game_state, dice_results):
-        pass
+        formatted_results = ' '.join(dice_results)
+        st.write(f"{self.name}, it's your turn. Dice results: {formatted_results}")
+        # Implement the logic for a human player's move
 
 class HumanPlayer(Player):
     def make_move(self, game_state, dice_results):
-        formatted_results = ''.join(dice_results)
+        formatted_results = ' '.join(dice_results)
         st.write(f"{self.name}, it's your turn. Dice results: {formatted_results}")
         # Implement the logic for a human player's move
 
 class AiPlayer(Player):
     def make_move(self, game_state, dice_results):
-        formatted_results = ''.join(dice_results)
+        formatted_results = ' '.join(dice_results)
         st.write(f"{self.name} is making a move... Dice results: {formatted_results}")
         # Implement the logic for an AI player's move
 
@@ -38,8 +40,9 @@ players = [human_player, ai_player]
 st.title("Qwixx Game Simulator")
 
 if st.button("Roll All Dice"):
-    dice_results = ['R', 'B', 'Y', 'G', 'W', 'W']
-    st.write("Dice results:", ''.join(dice_results))
+    colors = ['R', 'B', 'Y', 'G', 'W', 'W']
+    dice_results = [f"{color}{random.randint(1, 6)}" for color in colors]
+    st.write("Dice results:", ' '.join(dice_results))
     
     current_player = random.choice(players)  # Choose a random player
     game_state["current_player"] = current_player
