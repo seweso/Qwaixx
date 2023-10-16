a767459690cec34defb3c3c5faa29358235496f4
Wouter Schut <github@seweso.com>
2023-10-16 15:41:25 +0200

    Can you make the dice (and a to string function) a class? (and thus remove duplicate code)

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,25 +2,30 @@ import streamlit as st
 import random
 
 class Player:
-    def __init__(self, name):
+    def __init(self, name):
         self.name = name
 
     def make_move(self, game_state, dice_results):
         formatted_results = ' '.join(dice_results)
         st.write(f"{self.name}, it's your turn. Dice results: {formatted_results}")
-        # Implement the logic for a human player's move
+        # Implement the logic for a player's move
 
 class HumanPlayer(Player):
-    def make_move(self, game_state, dice_results):
-        formatted_results = ' '.join(dice_results)
-        st.write(f"{self.name}, it's your turn. Dice results: {formatted_results}")
-        # Implement the logic for a human player's move
+    pass
 
 class AiPlayer(Player):
-    def make_move(self, game_state, dice_results):
-        formatted_results = ' '.join(dice_results)
-        st.write(f"{self.name} is making a move... Dice results: {formatted_results}")
-        # Implement the logic for an AI player's move
+    pass
+
+class Dice:
+    def __init__(self, colors):
+        self.colors = colors
+        self.results = []
+
+    def roll(self):
+        self.results = [f"{color}{random.randint(1, 6)}" for color in self.colors]
+
+    def format_results(self):
+        return ' '.join(self.results)
 
 # Example game state
 game_state = {
@@ -33,6 +38,9 @@ game_state = {
 human_player = HumanPlayer("Alice")
 ai_player = AiPlayer("AI Bot")
 
+# Create dice
+qwixx_dice = Dice(['R', 'B', 'Y', 'G', 'W', 'W'])
+
 # List of players
 players = [human_player, ai_player]
 
@@ -40,10 +48,9 @@ players = [human_player, ai_player]
 st.title("Qwixx Game Simulator")
 
 if st.button("Roll All Dice"):
-    colors = ['R', 'B', 'Y', 'G', 'W', 'W']
-    dice_results = [f"{color}{random.randint(1, 6)}" for color in colors]
-    st.write("Dice results:", ' '.join(dice_results))
+    qwixx_dice.roll()
+    st.write("Dice results:", qwixx_dice.format_results())
     
     current_player = random.choice(players)  # Choose a random player
     game_state["current_player"] = current_player
-    current_player.make_move(game_state, dice_results)
+    current_player.make_move(game_state, qwixx_dice.results)
